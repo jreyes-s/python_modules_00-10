e7707b35d06c58b44ce1c2545f57827b9c9033df
jreyes-s <jreyes-s@student.42madrid.com>
2026-04-13 04:47:15 +0200

    Enhance Flower and Tree classes with improved blooming and shading functionality

--- python01/ex5/ft_plant_types.py
+++ python01/ex5/ft_plant_types.py
@@ -18,31 +18,39 @@ class Flower(Plant):
     def __init__(self, name: str, height: float, age: int, color: str) -> None:
         super().__init__(name, height, age)
         self.color = color
+        self.is_blooming = False
 
     def bloom(self) -> None:
-        self.is_blooming = False
-        print(f"[asking the rose to bloom]")
+        print(f"[asking the {self.name.lower()} to bloom]")
+        self.is_blooming = True
     
     def showing(self) -> None:
         print("=== Flower")
         print(super().showing())
         print(f" Color: {self.color}")
+        if self.is_blooming:
+            print(f"{self.name} is blooming beautifully")
+        else:
+            print(f"Rose has not bloomed yet")
 
 class Tree(Plant):
     def __init__(self, name: str, height: float, age: int, trunk_diameter: float) -> None:
         super().__init__(name, height, age)
         self.trunk_diameter = trunk_diameter
+        self.producing_shade = False
 
-    def produce_shade(self) -> bool:
-        if self.trunk_diameter >= 5:
-            print(f"[asking the oak to produce shade]")
-            print(f"Tree Oak now produces a shade of {self.height}cm long and {self.trunk_diameter}cm wide.")
-            return True
-        return False
+    def produce_shade(self) -> None:
+        print(f"[asking the {self.name.lower()} to produce shade]")
+        self.producing_shade = True
 
     def showing(self) -> None:
         print("=== Tree")
         print(super().showing())
+        print(f"Trunk diameter: {self.trunk_diameter}cm")
+        if self.producing_shade:
+            print(f"Tree Oak now produces a shade of {self.height}cm long and {self.trunk_diameter}cm wide")
+        else:
+            print(f"Tree Oak doesn't produce shade")
 
 class Vegetable(Plant):
     def __init__(self, name: str, height: float, age: int, harvest_season: str) -> None:
