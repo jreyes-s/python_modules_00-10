0b066c99082cbe59f6323d7c9ae24d61bfb61bd0
jreyes-s <jreyes-s@student.42madrid.com>
2026-04-17 19:52:35 +0200

    Mod

--- python01/ex6/ft_garden_analytics.py
+++ python01/ex6/ft_garden_analytics.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 
+class Plant:
+    def __init__(self, hola: str) -> None:
+        self.hola = hola
+
+    @staticmethod
+    def check_age(age: int) -> bool:
+        if age > 18:
+            return True
+        return False
+
+def ft_garden_analytics():
+    print("=== Garden statistics ===")
+    print("=== Check year-old")
+
+if __name__ == "__main__":
+    ft_garden_analytics()
