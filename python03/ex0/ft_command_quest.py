ef3d71e4f7fbbc2302d9c5aff011765a76fcdbeb
jreyes-s <jreyes-s@student.42madrid.com>
2026-04-20 23:07:58 +0200

    add: ex0

---
+++ python03/ex0/ft_command_quest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+
+def ft_args() -> None:
+    length = len(sys.argv)
+    if length == 1:
+        print("No arguments provided!")
+    elif length > 1:
+        print(f"Arguments received: {length - 1}")
+        for i in range(1, length):
+            print(f"Argument {i}: {sys.argv[i]}")
+    print(f"Total arguments: {length}\n")
+
+
+if __name__ == "__main__":
+    print("=== Command Quest ===")
+    print(f"Program name: {sys.argv[0]}")
+    ft_args()
