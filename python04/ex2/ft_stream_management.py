0765d328d0d5d1d3f08f1d7bf1eb35b8da7a4742
jreyes-s <jreyes-s@student.42madrid.com>
2026-05-26 16:21:32 +0200

    copilot desactivado

---
+++ python04/ex2/ft_stream_management.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def get_user_input(prompt: str) -> str:
+    """ Get the user input using low level streams """
+    sys.stdout.write(prompt)
+    sys.stdout.flush()
+    line = sys.stdin.readline()
+    if not line:
+        return ""
+    return line.rstrip("\n")
+
+
+def save_data(lines: list[str]) -> None:
+    """Save the data with the # (hashtag) at the end of the line"""
+    new_file = None
+    new_filename = get_user_input("Enter new file name (or empty): ")
+    if new_filename != None:
+        sys.stdout.write(f"Saving data to '{new_filename}'\n")
+    if new_filename == "" or " " in new_filename:
+        sys.stdout.write("Data not saved.")
+        sys.stdout.flush()
+        return
+    try:
+        new_file = open(new_filename, 'w')
+        for line in lines:
+            new_file.write(line)
+            sys.stdout.flush()
+    except Exception as e:
+        sys.stderr.write(f"[STDERR] Error saving data: {e}\n")
+        sys.stderr.flush()
+        sys.exit(1)
+    finally:
+        if new_file:
+            sys.stdout.write(f"Data saved in file '{new_filename}'\n")
+            sys.stdout.flush()
+            new_file.close()
+        else:
+            sys.stdout.write(f"Data not saved.")
+
+
+def process_data(original_lines: list[str]) -> None:
+    """Transform lines adding the # (hashtag) symbol at the end of the line"""
+    transformed_lines: list[str] = []
+
+    try:
+        sys.stdout.write("\nTransform data:\n")
+        sys.stdout.write("---\n\n")
+        for line in original_lines:
+            clean_line = line.rstrip("\n")
+            new_line = f"{clean_line}#\n"
+            sys.stdout.write(new_line)
+            transformed_lines.append(new_line)
+        sys.stdout.write("\n---\n")
+        sys.stdout.flush()
+        save_data(transformed_lines)
+    except Exception as e:
+        sys.stderr.write(f"[STDERR] Error processing data: {e}")
+        sys.stderr.flush()
+        sys.exit(1)
+
+
+def main() -> None:
+    args = sys.argv
+    if len(args) < 2 or len(args) > 2:
+        sys.stderr.write("[STDERR] Usage: ft_ancient_text.py <file>\n")
+        sys.stderr.flush()
+        sys.exit(1)
+
+    content = None
+    filename = sys.argv[1]
+
+    sys.stdout.write(f"Accessing file '{filename}'\n")
+    sys.stdout.flush()
+    try:
+        content = open(filename, 'r')
+        lines = content.readlines()
+        sys.stdout.write("---\n\n")
+        sys.stdout.flush()
+        for line in lines:
+            new_line = line.rstrip("\n")
+            sys.stdout.write(f"{new_line}\n")
+        sys.stdout.write("\n---\n")
+        sys.stdout.flush()
+    except (FileNotFoundError, PermissionError) as e:
+        sys.stderr.write(f"[STDERR] Error opening file '{filename}': {e}\n")
+        sys.stderr.flush()
+    finally:
+        if content:
+            sys.stdout.write(f"File '{filename}' closed.\n")
+            sys.stdout.flush()
+            content.close()
+            process_data(lines)
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    sys.stdout.write("=== Cyber Archives Recovery ===\n")
+    sys.stdout.flush()
+    main()
---
+++ python04/ex3/ft_vault_security.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def secure_archive(filename: str, action: str = 'r', content: str = "") -> tuple[bool, str]:
+    success: bool = False
+    tp: tuple[bool, str] = tuple()
+
+    try:
+        success = True
+        if (action == 'w'):
+            with open(content, action) as file:
+                file.write(content)
+                tp = (success, "Content successfully written to file")
+        else:
+            with open(filename, action) as file:
+                content = file.read()
+                tp = (success, str(content))
+    except (FileNotFoundError, PermissionError) as error:
+        success = False
+        tp = (success, str(error))
+    return (tp)
+
+
+if __name__ == "__main__":
+    print("=== Cyber Archives Security ===")
+
+    print("\nUsing 'secure_archive' to read from a nonexistent file:")
+    print(secure_archive('/non/existingfile'))
+
+    print("\nUsing 'secure_archive' to read from an inaccessible file:")
+    print(secure_archive('/etc/master.passwd'))
+
+    print("\nUsing 'secure_archive' to read from a regular file:")
+    print(secure_archive('../ex2/ancient_fragment.txt'))
+
+    print("\nUsing 'secure_archive' to write previous content to a new file:")
+    print(secure_archive('../ex2/ancient_fragment.txt', 'w', 'hola'))
